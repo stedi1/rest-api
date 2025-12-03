39e4d407e283d1384e574659a18c0e49d1397227
Eduard <stedi.str@gmail.com>
2025-12-03 10:22:37 +0300

    v0.0.1 API test

---
+++ restTest/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handleTime(res http.ResponseWriter, req *http.Request) {
+	s := time.Now().Format("02.01.2006 15:04:05")
+	res.Write([]byte(s))
+}
+
+func handleMain(res http.ResponseWriter, req *http.Request) {
+	s := fmt.Sprintf("Method: %s\nHost: %s\nPath: %s",
+		req.Method, req.Host, req.URL.Path)
+	res.Write([]byte(s))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/time", handleTime)
+	mux.HandleFunc("/", handleMain)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
